c88b42a7ad9610d8ff4a827c6b6683bf447ad9dc
Ruggero Russo <ruggero.russo89@gmail.com>
2016-12-31 14:51:01 +0100

    Enabled profiles for debuggin purpose

--- src/main/java/net/ruj/cloudfuse/fuse/FileSystemService.java
+++ src/main/java/net/ruj/cloudfuse/fuse/FileSystemService.java
@@ -1,15 +1,16 @@
 package net.ruj.cloudfuse.fuse;
 
-import net.ruj.cloudfuse.gdrive.GDriveService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.nio.file.Paths;
 
 @Service
+@Profile("linux")
 public class FileSystemService {
     static final Logger logger = LoggerFactory.getLogger(FileSystemService.class);
     private final MemoryFS memoryFS;
