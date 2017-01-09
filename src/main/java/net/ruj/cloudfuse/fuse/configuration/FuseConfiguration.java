3e4128d42c1d24adb4eca12f891bf57bd3e22314
Ruggero Russo <ruggero.russo89@gmail.com>
2017-01-09 12:11:57 +0100

    Changing configuration indicating they're defaults

--- src/main/java/net/ruj/cloudfuse/fuse/configuration/FuseConfiguration.java
+++ src/main/java/net/ruj/cloudfuse/fuse/configuration/FuseConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "cloudfuse.fuse")
+@ConfigurationProperties(prefix = "cloudfuse.fuse.defaults")
 public class FuseConfiguration {
     private DriveConfiguration drive;
 
